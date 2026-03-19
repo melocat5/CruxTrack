64b12927a7c8e83e9db997b3e044089653a35312
melocat5 <a.teschner1025@gmail.com>
2026-03-19 16:06:43 -0400

    simple login and dash for frontend
    login page and dashboard to get started with
    nothing set in stone, just a base for us to work off of

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/login/login';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard }
+];
---
+++ frontend/src/app/features/login/login.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  loginForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Handle login logic here
+      this.router.navigate(['/dashboard']);
+    }
+  }
+}
+
